8239a2ed2b00f4a820ce7b32ac2ea7ab3c401a87
Ella <efb4518@ic.ac.uk>
2020-06-25 18:06:05 +0100

    Highlight icon on selected

--- app/src/main/java/com/example/psychapp/elements/NavegationBar.java
+++ app/src/main/java/com/example/psychapp/elements/NavegationBar.java
@@ -2,8 +2,11 @@ package com.example.psychapp.elements;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.text.Layout;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,52 +31,74 @@ public class NavegationBar {
 
     public NavegationBar(AppCompatActivity packagageContext, ViewGroup navegationBar) {
 
-        int BUTTON_WIDTH = 50;
-
-        Resources r = packagageContext.getResources();
-        float px = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                BUTTON_WIDTH,
-                r.getDisplayMetrics()
-        );
+//        int BUTTON_WIDTH = 70;
+//
+//        Resources r = packagageContext.getResources();
+//        float px = TypedValue.applyDimension(
+//                TypedValue.COMPLEX_UNIT_DIP,
+//                BUTTON_WIDTH,
+//                r.getDisplayMetrics()
+//        );
 
         this.packagageContext = packagageContext;
         this.navegationBar = navegationBar;
         ImageButton image;
         //TODO: extract duplication
-        System.out.printf("\n\n ##################### PACKAGE NAME %s \n\n", packagageContext.getLocalClassName());
+        Log.d("ifris", "\n\n ##################### PACKAGE NAME %s "+ packagageContext.getLocalClassName() + "\n-\n-\n");
         switch (packagageContext.getLocalClassName()) {
-            case ("com.example.psychapp.activities.HomeScreen"):
+            case ("activities.HomeScreen"):
+                Log.d("TAG", "-\n-\n-\nACTIVTY HOME SCREEN\n-\n-\n-\n");
                 image = navegationBar.findViewById(R.id.home_button);
-                image.setImageResource(R.drawable.home);
-                image.setImageTintList(null);
+//                image.setImageResource(R.drawable.homeicon_ul);
+//                image.setImageTintList(null);
+                image.setImageTintList(ColorStateList.valueOf(Color.WHITE));
 
-                ViewGroup.LayoutParams params = image.getLayoutParams();
-                params.width = (int) px;
-                image.setLayoutParams(params);
+//                ViewGroup.LayoutParams params = image.getLayoutParams();
+//                params.width = (int) px;
+//                image.setLayoutParams(params);
                 image.requestLayout();
 
                 break;
             //TODO: set width using weird width method and weight = 0
-            case ("com.example.psychapp.activities.OverdoseSelector"):
+            case ("activities.OverdoseSelector"):
+            case("activities.OverdoseInfo"):
+                Log.d("TAG", "-\n-\n-\nACTIVTY OVERDOSE SELECTOR SCREEN\n-\n-\n-\n");
                 image = navegationBar.findViewById(R.id.od_button);
-                image.setImageResource(R.drawable.overdose2);
-                image.setImageTintList(null);
+//                image.setImageResource(R.drawable.odicon_ul);
+//                image.setImageTintList(null);
+                image.setImageTintList(ColorStateList.valueOf(Color.WHITE));
 
-                ViewGroup.LayoutParams params2 = image.getLayoutParams();
-                params2.width = (int) px;
-                image.setLayoutParams(params2);
+//                ViewGroup.LayoutParams params2 = image.getLayoutParams();
+//                params2.width = (int) px;
+//                image.setLayoutParams(params2);
                 image.requestLayout();
                 break;
 
-            case ("com.example.psychapp.activities.PillReports"):
+            case ("activities.PillReports"):
+            case ("activities.AdvancesPillSearch"):
+                Log.d("TAG", "-\n-\n-\nACTIVTY PILL REPORTS SCREEN\n-\n-\n-\n");
                 image = navegationBar.findViewById(R.id.pill_button);
-                image.setImageResource(R.drawable.pill_exp);
-                image.setImageTintList(null);
+//                image.setImageResource(R.drawable.pillicon_ul);
+//                image.setImageTintList(null);
+                image.setImageTintList(ColorStateList.valueOf(Color.WHITE));
 
-                ViewGroup.LayoutParams params3 = image.getLayoutParams();
-                params3.width = (int) px;
-                image.setLayoutParams(params3);
+//                ViewGroup.LayoutParams params3 = image.getLayoutParams();
+//                params3.width = (int) px;
+//                image.setLayoutParams(params3);
+                image.requestLayout();
+                break;
+
+            case("activities.ExperienceReportSearch"):
+            case("activities.ExperienceReport"):
+                Log.d("TAG", "-\n-\n-\nACTIVTY EXPERIENCE REPORTS SCREEN\n-\n-\n-\n");
+                image = navegationBar.findViewById(R.id.experienceButton);
+//                image.setImageResource(R.drawable.book_ul);
+//                image.setImageTintList(null);
+                image.setImageTintList(ColorStateList.valueOf(Color.WHITE));
+
+//                ViewGroup.LayoutParams params4 = image.getLayoutParams();
+//                params4.width = (int) px;
+//                image.setLayoutParams(params4);
                 image.requestLayout();
                 break;
         }
